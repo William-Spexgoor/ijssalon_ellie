52aaac08e9edc0d994ab853ea1ddaa62b6d7e4a3
William Spexgoor <williamspexgoor@gmail.com>
2025-05-11 11:20:33 +0200

    bestand tijdelijk.py gecodeerd

--- tijdelijk.py
+++ tijdelijk.py
@@ -1,19 +1,21 @@
-mijn_lijst = ["aarbei" , "vanille" , "chocolade"]
+def print_aanbieding():
+    prijzen = {
+        "aardbei": 3,
+        "vanille": 4,
+        "chocolade": 5
+    }
+    aanbieding = prijzen["aardbei"] * 0.8
 
-mijn_dictonairy = {
-    "aarbei": 3,#6
-    "vanille": 4,#7
-    "chocolade": 5#9
-}
-aanbieding = mijn_dictonairy["aarbei"] * 0.8
+    reclame_tekst = f"Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts € {aanbieding}"
+    reclame_tekst2 = reclame_tekst[:63]
+    reclame_tekst3 = reclame_tekst2.upper()
+    reclame_tekst4 = reclame_tekst3.split()
 
-reclame_tekst = f"vandaag in de aanbieding vanille ijs, 1 liter - slechts {aanbieding}"
-reclame_tekst2 = reclame_tekst[:reclame_tekst.index("0")]
-reclame_tekst3 = f"vandaag in de aanbieding vanille ijs, 1 liter - slechts {aanbieding}"
-reclame_tekst4 = reclame_tekst3.split()
+    for el in reclame_tekst4:
+        if len(el) > 4:
+            print(el.upper())
+        else:
+            print(el.lower())
 
-mijn_lijst = ["mijn_dictonairy"] 
-for el in mijn_dictonairy:
-    if len (el) >= 5:
-        print(el.upper())
-    else:print(el.lower())
+print_aanbieding()
+
